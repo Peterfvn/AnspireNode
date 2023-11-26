cd98e358f8ddb5d8609441bbee40f1716ec5dbb7
pfvannes <pfvannes@go.olemiss.edu>
2023-11-26 16:34:48 -0600

    Added localhost to CORS for testing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { upload, processCSVUpload } from './processCSVUpload.js'
 
 const app = express();
 app.use(cors({
-    origin: ["https://anspire.onrender.com"],
+    origin: ["https://anspire.onrender.com", "http://localhost:5173"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
     optionsSuccessStatus: 204
